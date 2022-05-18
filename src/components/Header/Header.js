7eec87f6e55b53f5b2ca3355f99afd25d09caf8c
= <dco3@cin.ufpe.br>
2022-05-18 12:58:04 -0300

    header ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import { Menu, Search, User } from 'react-feather';
+import React from "react";
+import styled from "styled-components/macro";
+import { Menu, Search, User } from "react-feather";
 
-import { QUERIES } from '../../constants';
+import { FAMILIES, QUERIES } from "../../constants";
 
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,16 +29,60 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <IconsWrapper>
+          <ActionGroup>
+            <button>
+              <Search size={24} />
+            </button>
+            <button>
+              <Menu size={24} />
+            </button>
+          </ActionGroup>
+        </IconsWrapper>
+
         <Logo />
+        <ButtonWrapper>
+          <Button>Subscribe</Button>
+          <a href="/#">Already a subscriber?</a>
+        </ButtonWrapper>
       </MainHeader>
     </header>
   );
 };
 
+const ButtonWrapper = styled.div`
+  display: none;
+  place-content: center;
+  justify-self: end;
+  gap: 8px;
+  & a {
+    font-family: ${FAMILIES.serif};
+    font-style: italic;
+    font-weight: 400;
+    font-size: 14px;
+    text-align: center;
+
+    text-decoration-line: underline;
+    color: var(--gray-900);
+  }
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+  }
+`;
+const IconsWrapper = styled.div`
+  display: none;
+  @media ${QUERIES.laptopAndUp} {
+    display: block;
+  }
+`;
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -60,11 +104,16 @@ const ActionGroup = styled.div`
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
+  display: grid;
+  grid-template-columns: 100%;
   align-items: center;
   justify-content: center;
+  place-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
 
 export default Header;
